e01b62a72c5fad9c379843a04840897b68a2a207
milovanovmaksim <milovanov160386@gmail.com>
2024-08-26 16:38:02 +0300

    change commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from stress_strain_curve import StressStraineCurve
 
 
 def main():
-    sigma_ys = 190.32 # Engineering yield stress evaluated at the temperature of interest  [МПа]
-    sigma_uts = 403.38 # Engineering ultimate tensile stress evaluated at the temperature of interest  [МПа]
-    Ey = 198.74E+3 # Modulus of elasticity evaluated at the temperature of interest Мпа
+    sigma_ys = 190.32 # Engineering yield stress evaluated at the temperature of interest  [MPa].
+    sigma_uts = 403.38 # Engineering ultimate tensile stress evaluated at the temperature of interest  [MPa].
+    Ey = 198.74E+3 # Modulus of elasticity evaluated at the temperature of interest [MPa].
     epsilon_p = 0
     m2 = 0
 
